95bc56857bfa0c4c788033468a682d7c92b9960f
Sushmita Ghosh <sush7879ghosh@gmail.com>
2021-03-01 00:50:44 +0530

    Add files via upload

---
+++ SumOfArray.java
@@ -0,0 +1,17 @@
+package code_probs;
+import java.util.Arrays;
+import java.util.Scanner;
+public class SumOfArray {
+   public static void main(String args[]){
+      
+      Scanner s = new Scanner(System.in);
+      int size = s.nextInt();
+      int myArray[] = new int [size];
+      int sum = 0;
+      for(int i=0; i<size; i++){
+         myArray[i] = s.nextInt();
+         sum = sum + myArray[i];
+      }
+      System.out.println(sum);
+   }
+}
